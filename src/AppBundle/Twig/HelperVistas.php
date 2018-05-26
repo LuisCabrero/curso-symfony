718da104053252ee3d2fa24e946c53af9c197a52
LuisCabrero <100330021@alumnos.uc3m.es>
2018-05-26 15:44:05 +0200

    Funciones en twig

---
+++ src/AppBundle/Twig/HelperVistas.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Twig;
+
+class HelperVistas extends \TwigExtension{
+
+	public function getFunctions(){
+		return array(
+			'generateTable' => new \Twig_Function_Method($this, 'generateTable')
+		);
+	}
+
+
+	public function generateTable($num_columns, $num_rows){
+		$tabla = "<table class='table'>";
+		for ($i=0; $i <= $num_rows ; $i++) { 
+			$tabla .= "<tr>";
+			for ($j=0; $j <= $num_columns ; $j++) { 
+				$table .= "<td>Columna</td>";
+			}
+			$tabla .= "</tr>";
+		}
+		$tabla .= "</table>";
+		return $tabla;
+	}
+
+	public function getName(){
+		return "app_bundle";
+	}
+
+}
