134b16c7fbe085e89ac17e7f2232c24a4d7244dc
LuisCabrero <100330021@alumnos.uc3m.es>
2018-05-26 14:16:37 +0200

    Pruebas con bucles en twig

--- src/AppBundle/Controller/PruebasController.php
+++ src/AppBundle/Controller/PruebasController.php
@@ -15,9 +15,21 @@ class PruebasController extends Controller
         var_dump($request->get("holaPost"));
         exit;*/
 
+        $productos  = array(
+                    array("producto"=>"Consola", "precio"=>2),
+                    array("producto"=>"Consola 2", "precio"=>3),
+                    array("producto"=>"Consola 3", "precio"=>4),
+                    array("producto"=>"Consola 4", "precio"=>5),
+                    array("producto"=>"Consola 5", "precio"=>6),
+                    array("producto"=>"Consola 6", "precio"=>7)
+        );
+        $fruta = array("manzana"=>"golden", "pera"=>"rica");
+
         // replace this example code with whatever you need
         return $this->render('AppBundle:Pruebas:index.html.twig', array(
             'texto' => $name." ".$page,
+            'productos' => $productos,
+            'fruta' => $fruta
         ));
     }
 }
