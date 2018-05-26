3aa65e1580211242fd01cfcd3749ca5fa0a1aa0d
LuisCabrero <100330021@alumnos.uc3m.es>
2018-05-26 13:24:50 +0200

    Parámetros por GET y POST

--- src/AppBundle/Controller/PruebasController.php
+++ src/AppBundle/Controller/PruebasController.php
@@ -11,6 +11,10 @@ class PruebasController extends Controller
 
     public function indexAction(Request $request, $name, $page)
     {
+        var_dump($request->query->get("hola"));
+        var_dump($request->get("holaPost"));
+        exit;
+
         // replace this example code with whatever you need
         return $this->render('AppBundle:Pruebas:index.html.twig', array(
             'texto' => $name." ".$page,
